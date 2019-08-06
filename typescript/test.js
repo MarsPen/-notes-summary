63ea280dc2c6b01805eec41aea6ad67ddd03252b
1605275889@qq.com <MarsPen@users.noreply.github.com>
2019-08-06 18:00:56 +0800

    更新ts

---
+++ typescript/test.js
@@ -0,0 +1,8 @@
+var People = /** @class */ (function () {
+    function People(name) {
+        this.name = name;
+    }
+    return People;
+}());
+var people = new People('renbo');
+console.log(people.name); // renbo
--- typescript/test.ts
+++ typescript/test.ts
@@ -1,26 +1,9 @@
-// 定义泛型结构
-interface CreatePeopleFunc {
-  <T>(name: string, age: T): Array<T>;
-}
-
-// 创建泛型
-let createPeople:CreatePeopleFunc;
-let people: T[] = []
-createPeople = function<T>(name: string, age: T): Array<T> {
-  
-  let temp:any = {
-    name : name,
-    age: age
+class People {
+  public name;
+  public constructor(name) {
+    this.name = name;
   }
-  people.push(temp)
-  return people
 }
-// 调用函数
-createPeople('zhangsan', 28)
-// 调用函数
-createPeople('', 28)
 
-let obj = {
-  ['name': '1111','age': 23],
-  ['name': 'lisi','age': '29']
-}
+let people = new People('renbo');
+console.log(people.name); // renbo
